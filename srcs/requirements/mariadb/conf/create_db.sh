fdf6b1d3a367e3b42ad29d1c653e1968786ea7dc
Oussama Belhaj <oubelhaj@c4r1p8.1337.ma>
2024-02-22 16:53:21 +0100

    database not shown in table problem

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,12 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
-#db_name = Database Name
-#db_user = User
-#db_pwd = User Password
-
-echo "CREATE DATABASE IF NOT EXISTS wordpress ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'wppass' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%' ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
