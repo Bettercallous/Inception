539125a22a75a3ce6ac5af90da11c7e67e252dfa
Bettercallous <bettercallous@gmail.com>
2024-02-24 21:25:23 +0100

    fixed mariadb 2

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -5,6 +5,4 @@ echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mysql < db1.sql
-
 mysqld_safe
