9b19e745b73bf4fc9b13ce5ad863b88d3ca3ea58
Oussama Belhaj <oubelhaj@c4r2p1.1337.ma>
2024-02-28 01:58:58 +0100

    fixed mariadb - changed script logic

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+service mariadb start
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME ;"
+mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;"
+mariadb -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
 
 mysqld_safe
