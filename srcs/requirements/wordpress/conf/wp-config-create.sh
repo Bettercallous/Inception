3cda50919078654241b3d2bf6399b91d42837bb5
Bettercallous <oubelhaj@1337.ma>
2024-03-08 16:27:06 +0100

    working on redis

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -5,7 +5,7 @@ mkdir /var/www/html
 
 cd /var/www/html
 
-rm -rf *
+rm -rf /var/www/html/*
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
@@ -22,14 +22,22 @@ sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_PASS/" wp-config.php
 sed -i "s/localhost/$DB_HOST/1" wp-config.php
 
+echo "define( 'WP_REDIS_HOST', 'redis' );" >> wp-config.php
+echo "define( 'WP_REDIS_PORT', 6379 );" >> wp-config.php
+echo "define( 'WP_CACHE', true );" >> wp-config.php
+
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 wp theme install musicmantra --activate --allow-root
 
+wp plugin install redis-cache --activate --allow-root
+
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir /run/php
+
+wp redis enable --allow-root
 
 php-fpm7.4 -F
