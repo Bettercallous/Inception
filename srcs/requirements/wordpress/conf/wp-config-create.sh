4b5d39def842ad998ca6ad74e1adb8e8a8a16d5a
Bettercallous <oubelhaj@1337.ma>
2024-03-13 02:35:29 +0000

    updated wordpress

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -15,24 +15,21 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
-rm /var/www/html/wp-config-sample.php
-
-mv /wp-config.php /var/www/html/wp-config.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 sed -i "s/database_name_here/$DB_NAME/" wp-config.php
 sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_PASS/" wp-config.php
 sed -i "s/localhost/$DB_HOST/1" wp-config.php
 
-sed -i "s/redis_host_here/$REDIS_HOST/" wp-config.php
-sed -i "s/redis_port_here/$REDIS_PORT/" wp-config.php
+sed -i "86 i define( 'WP_REDIS_HOST', '$REDIS_HOST' );" wp-config.php
+sed -i "87 i define( 'WP_REDIS_PORT', $REDIS_PORT );" wp-config.php
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-# wp theme install musicmantra --activate --allow-root
-wp theme install oaknut --activate --allow-root
+wp theme install astra --activate --allow-root
 
 wp plugin install redis-cache --activate --allow-root
 
