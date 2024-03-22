5a42486c76d5d0a056eed1c1f8e588100abba217
oubelhaj <90373269+Bettercallous@users.noreply.github.com>
2024-03-22 01:17:01 +0000

    fixed typo

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -20,7 +20,7 @@ mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i "s/database_name_here/$DB_NAME/" wp-config.php
 sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_PASS/" wp-config.php
-sed -i "s/localhost/$DB_HOST/1" wp-config.php
+sed -i "s/localhost/$DB_HOST/" wp-config.php
 
 sed -i "86 i define( 'WP_REDIS_HOST', '$REDIS_HOST' );" wp-config.php
 sed -i "87 i define( 'WP_REDIS_PORT', $REDIS_PORT );" wp-config.php
